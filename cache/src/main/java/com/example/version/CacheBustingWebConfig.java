1b4fb884286742eedd9ab5cbc2f29fe70fdbd9dd
yukong <kyb062666@gmail.com>
2022-09-02 17:48:31 +0900

    feat: 정적 파일에 한해서 캐시 무효화 설정 추가

--- cache/src/main/java/com/example/version/CacheBustingWebConfig.java
+++ cache/src/main/java/com/example/version/CacheBustingWebConfig.java
@@ -1,14 +1,18 @@
 package com.example.version;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CacheBustingWebConfig implements WebMvcConfigurer {
 
-    public static final String PREFIX_STATIC_RESOURCES = "/resources";
+    public static final String PREFIX_STATIC_RESOURCES = "/resources/";
+    private static final CacheControl RESOURCE_CACHE_CONTROL = CacheControl.maxAge(Duration.ofDays(365)).cachePublic();
 
     private final ResourceVersion version;
 
@@ -19,7 +23,8 @@ public class CacheBustingWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/**")
-                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler(PREFIX_STATIC_RESOURCES + version.getVersion() + "/**")
+                .addResourceLocations("classpath:/static/")
+                .setCacheControl(RESOURCE_CACHE_CONTROL);
     }
 }
