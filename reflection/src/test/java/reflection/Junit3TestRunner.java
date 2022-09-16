ddaa1da73c1fbd705a69131e290be083abb5d2e6
yukong <kyb062666@gmail.com>
2022-09-16 14:56:30 +0900

    test: Junit3TestRunner 통과하도록 수정

--- reflection/src/test/java/reflection/Junit3TestRunner.java
+++ reflection/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,28 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
+        Object junit3Test = new Junit3Test();
         Class<Junit3Test> clazz = Junit3Test.class;
 
         // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        Arrays.stream(methods)
+            .filter(method -> method.getName().startsWith("test"))
+            .forEach(method -> {
+                try {
+                    method.invoke(junit3Test, null);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            });
     }
 }
