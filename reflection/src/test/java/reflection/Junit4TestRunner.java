2f849c7fe39b4b67c2256569b6d055208d3d68a4
yukong <kyb062666@gmail.com>
2022-09-16 15:04:06 +0900

    test: Junit4TestRunner 통과하도록 수정

--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,28 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
+        Object junit4Test = new Junit4Test();
         Class<Junit4Test> clazz = Junit4Test.class;
 
         // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Method[] methods = clazz.getMethods();
+        Arrays.stream(methods)
+            .filter(method -> method.isAnnotationPresent(MyTest.class))
+            .forEach(method -> {
+                try {
+                    method.invoke(junit4Test, null);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            });
     }
 }
