b7cb298600883da4861cd8894fabdae8382d2ee1
yukong <kyb062666@gmail.com>
2022-09-28 11:12:34 +0900

    feat: 4단계 DI Container 구현

--- di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,24 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        return scanClasses(packageName, List.of(Repository.class, Service.class));
+    }
+
+    private static Set<Class<?>> scanClasses(final String packageName, final List<Class<? extends Annotation>> annotations) {
+        final Reflections reflections = new Reflections(packageName);
+        final Set<Class<?>> classes = new HashSet<>();
+        for (final Class<? extends Annotation> annotation : annotations) {
+            classes.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+        return classes;
     }
 }
